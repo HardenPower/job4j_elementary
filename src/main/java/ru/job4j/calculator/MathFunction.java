6ab57d7c9d05841a455399b209ddf85cc3bb2ae6
HardenPower <a.boot96@gmail.com>
2024-03-20 16:46:46 +0300

    4.3. Вызов метода без указания класса [#504945] - убраны многострочные комментарии

--- src/main/java/ru/job4j/calculator/MathFunction.java
+++ src/main/java/ru/job4j/calculator/MathFunction.java
@@ -10,15 +10,9 @@ public class MathFunction {
     }
 
     public static void main(String[] args) {
-        int result1 = MathFunction.func1(3);
-        int result2 = MathFunction.func2(5);
-
-        /*
-        * warning в checkstyle плагине, если не использовать переменную result3
-        * как я понял задание, result3 не учавствует в переменной total при сложении
-        * но для исправлении предупреждения в checkstyle - добавил использование result3
-        */
-        int result3 = MathFunction.func1(100);
+        int result1 = func1(3);
+        int result2 = func2(5);
+        int result3 = func1(100);
         int total = result1 + result2 + result3;
         System.out.println(total);
     }
