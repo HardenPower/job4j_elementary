ccff3d3bf25f75eb038662617cf0e9ff22e6aebb
HardenPower <a.boot96@gmail.com>
2024-04-02 17:45:41 +0300

    4.2. Тернарное сравнение [#189]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Max {
+
+    public static int max(int left, int right) {
+        return left > right ? left : right;
+    }
+
+    public static void main(String[] args) {
+        int result = max(1, 2);
+        System.out.println(result);
+        result = max(5, 0);
+        System.out.println(result);
+        result = max(10, 10);
+        System.out.println(result);
+    }
+}
