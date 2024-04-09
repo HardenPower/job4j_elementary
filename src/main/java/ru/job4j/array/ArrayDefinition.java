df0623a3bb8ae663ac313efb494b4f7e155f94fc
HardenPower <a.boot96@gmail.com>
2024-04-09 19:01:13 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Karl Zeis";
+        names[1] = "Leo Messi";
+        names[2] = "Shaq O'Neil";
+        names[3] = "Shawn Penn";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
