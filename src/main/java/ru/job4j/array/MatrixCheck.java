47bd3ad888fcaeef558b93fc2c5005f361dd0200
HardenPower <a.boot96@gmail.com>
2024-04-25 18:22:35 +0300

    6.7.1. Моно строка в матрице. [#214126 #492778]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,30 +3,11 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int indx = row; indx < board.length; indx++) {
-            if (board[row][indx] == ' ') {
+        for (int indx = 0; indx < board.length; indx++) {
+            if (board[row][indx] != 'X') {
                 result = false;
                 break;
             }
-        }
-        return result;
-    }
-
-    public static boolean monoVertical(char[][] board, int column) {
-        boolean result = true;
-        for (int idx = column; idx < board.length; idx++) {
-            if (board[idx][column] == ' ') {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
-
-    public static char[] extractDiagonal(char[][] board) {
-        char[] result = new char[board.length];
-        for (int idx = 0; idx < result.length; idx++) {
-            result[idx] = board[idx][idx];
         }
         return result;
     }
--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -9,82 +9,10 @@ public class MatrixCheckTest {
         char[][] input = {
                 {' ', ' ', ' '},
                 {'X', 'X', 'X'},
-                {' ', ' ', ' '}
-        };
-        int row = 1;
-        boolean result = MatrixCheck.monoHorizontal(input, row);
-        assertThat(result).isTrue();
-    }
-
-    @Test
-    public void whenHasMonoHorizontal2() {
-        char[][] input = {
-                {' ', ' ', ' '},
-                {'X', 'X', ' '},
                 {' ', ' ', ' '},
         };
         int row = 1;
         boolean result = MatrixCheck.monoHorizontal(input, row);
-        assertThat(result).isFalse();
-    }
-
-    @Test
-    public void whenHasMonoVertical() {
-        char[][] input = {
-                {' ', ' ', 'X'},
-                {' ', ' ', 'X'},
-                {' ', ' ', 'X'},
-        };
-        int column = 2;
-        boolean result = MatrixCheck.monoVertical(input, column);
         assertThat(result).isTrue();
-    }
-
-    @Test
-    public void whenHasMonoVertical2() {
-        char[][] input = {
-                {' ', ' ', 'X'},
-                {' ', ' ', 'X'},
-                {' ', ' ', ' '},
-        };
-        int column = 2;
-        boolean result = MatrixCheck.monoVertical(input, column);
-        assertThat(result).isFalse();
-    }
-
-    @Test
-    public void whenDiagonalFullX() {
-        char[][] input = {
-                {'X', ' ', ' '},
-                {' ', 'X', ' '},
-                {' ', ' ', 'X'},
-        };
-        char[] result = MatrixCheck.extractDiagonal(input);
-        char[] expected = {'X', 'X', 'X'};
-        assertThat(result).containsExactly(expected);
-    }
-
-    @Test
-    public void whenDiagonalFullOne() {
-        char[][] input = {
-                {'1', ' ', ' '},
-                {' ', '1', ' '},
-                {' ', ' ', '1'},
-        };
-        char[] result = MatrixCheck.extractDiagonal(input);
-        char[] expected = {'1', '1', '1'};
-        assertThat(result).containsExactly(expected);
-    }
-
-    @Test
-    public void whenDiagonalMix() {
-        char[][] input = {
-                {'X', ' ', ' '},
-                {' ', 'Y', ' '},
-                {' ', ' ', 'Z'},
-        };
-        char[] result = MatrixCheck.extractDiagonal(input);
-        char[] expected = {'X', 'Y', 'Z'};
-        assertThat(result).containsExactly(expected);
     }
 }
