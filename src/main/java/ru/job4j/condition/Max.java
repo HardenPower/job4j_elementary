8119cc6f6b80ff6bb84e8fb141cad866c45b3e5b
HardenPower <a.boot96@gmail.com>
2024-06-16 15:28:07 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -6,6 +6,40 @@ public class Max {
         return left > right ? left : right;
     }
 
+    public static int max(int left, int middle, int right) {
+        int result;
+        if (left > middle && left > right) {
+            result = left;
+        } else if (middle > left && middle > right) {
+            result = middle;
+        } else {
+            result = right;
+        }
+        return result;
+    }
+
+    public static int max(int a, int b, int c, int d) {
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        if (d > max) {
+            max = d;
+        }
+        return max;
+    }
+
+    public static int max(short left, int right) {
+        return left > right ? left : right;
+    }
+
+    public static int max(short left, short right) {
+        return left > right ? left : right;
+    }
+
     public static void main(String[] args) {
         int result = max(1, 2);
         System.out.println(result);
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,43 @@ class MaxTest {
         int expected = 10;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenMax1To2To3Then3() {
+        int left = 1;
+        int right = 3;
+        int middle = 2;
+        int result = Max.max(left, middle, right);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax1To2To3To100Then100() {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 100;
+        int result = Max.max(a, b, c, d);
+        int expected = 100;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void when10To12Then12() {
+        short left = 10;
+        int right = 12;
+        int result = Max.max(left, right);
+        int expected = 12;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void when100To120Then120() {
+        short left = 100;
+        short right = 120;
+        int result = Max.max(left, right);
+        int expected = 120;
+        assertThat(result).isEqualTo(expected);
+    }
 }
